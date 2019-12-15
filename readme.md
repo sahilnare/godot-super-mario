03a8f501632e367c00ef99325cab97f70a0151d3
sahilnare <sahilnare78@gmail.com>
2019-12-15 21:49:56 +0530

    first commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+# Godot Super Mario Clone
---
+++ src/Actors/Actor.gd
@@ -0,0 +1,13 @@
+extends KinematicBody2D
+
+class_name Actor
+
+export var speed:= Vector2(300.0, 1000.0)
+export var gravity: = 3000.0
+
+var velocity: = Vector2.ZERO
+
+func _physics_process(delta: float) -> void:
+	velocity.y += gravity * delta
+	#velocity.y = max(velocity.y, speed.y)
+	velocity = move_and_slide(velocity)
