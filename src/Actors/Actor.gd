c12b9e3e25c0d5885373f0c3c910c6413697fe65
sahilnare <sahilnare78@gmail.com>
2019-12-15 23:29:30 +0530

    Added input for player

--- src/Actors/Actor.gd
+++ src/Actors/Actor.gd
@@ -2,13 +2,14 @@ extends KinematicBody2D
 
 class_name Actor
 
+const FLOOR_NORMAL: = Vector2.UP
+
 export var speed:= Vector2(300.0, 1000.0)
 export var gravity: = 3000.0
 
 
 var velocity: = Vector2.ZERO
 
-func _physics_process(delta: float) -> void:
-	velocity.y += gravity * delta
+#func _physics_process(delta: float) -> void:
+	
 	#velocity.y = max(velocity.y, speed.y)
-	velocity = move_and_slide(velocity)
