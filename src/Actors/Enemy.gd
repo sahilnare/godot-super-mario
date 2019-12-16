3ce4e40c4055fb8ddb51f8bd095c7caf7d524177
sahilnare <sahilnare78@gmail.com>
2019-12-16 21:02:17 +0530

    Added enemy death and camera

--- src/Actors/Enemy.gd
+++ src/Actors/Enemy.gd
@@ -5,8 +5,16 @@ func _ready() -> void:
 	set_physics_process(false)
 	_velocity.x = -speed.x
 
+func _on_StompDetector_body_entered(body: PhysicsBody2D) -> void:
+	if body.global_position.y > get_node("StompDetector").global_position.y:
+		return
+	get_node("CollisionShape2D").disabled = true;
+	queue_free()
+
 func _physics_process(delta: float) -> void:
 	_velocity.y += gravity * delta
 	if is_on_wall():
 		_velocity.x *= -1.0
 	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
+
+
